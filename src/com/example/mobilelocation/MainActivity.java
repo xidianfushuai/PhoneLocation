c53d715f167d14353b11a1354c5a6e9c0c1b451f
xidianfushuai <18792514506@163.com>
2016-05-06 11:00:15 +0800

    手机定位

---
+++ src/com/example/mobilelocation/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.mobilelocation;
+
+import java.util.List;
+
+import android.app.Activity;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+	private TextView tvLocation;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		tvLocation = (TextView) findViewById(R.id.tv_location);
+		//获取系统定位服务
+		LocationManager lm = (LocationManager) getSystemService(LOCATION_SERVICE);
+		//List<String> allProviders = lm.getAllProviders();//获得所以位置提供者
+		MyLocationListener listener = new MyLocationListener();
+		lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 60, 50, listener);//位置提供者  最短更新时间    最短更新距离
+		
+	}
+	class MyLocationListener implements LocationListener {
+
+		@Override
+		public void onLocationChanged(Location location) {
+			//位置发生变化
+			String j = "经度： " + location.getLongitude();
+			String w = "纬度： " + location.getLatitude();
+			String accuracy = "精确度： " + location.getAccuracy();
+			String altitude = "海拔： " + location.getAltitude();
+			tvLocation.setText(j + "\n" + w + "\n" + accuracy + "\n" + altitude);
+		}
+
+		@Override
+		public void onStatusChanged(String provider, int status, Bundle extras) {
+			//位置提供者的状态发生变化
+		}
+
+		@Override
+		public void onProviderEnabled(String provider) {
+			//打开GPS时
+		}
+
+		@Override
+		public void onProviderDisabled(String provider) {
+			//关闭GPS时
+		}
+		
+	}
+	
+}
